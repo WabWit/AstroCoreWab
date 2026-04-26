15363a71a443c8f2cb58ce2f7a7e402b72ce218e
HazeVista <zetateamhazevista@gmail.com>
2026-04-26 17:16:07 +0000

    0.1.4 Haze new apothecary

---
+++ src/main/java/com/astro/core/common/data/block/AstroApothecaryBlock.java
@@ -0,0 +1,11 @@
+package com.astro.core.common.data.block;
+
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import vazkii.botania.common.block.PetalApothecaryBlock;
+
+public class AstroApothecaryBlock extends PetalApothecaryBlock {
+
+    public AstroApothecaryBlock(BlockBehaviour.Properties props) {
+        super(Variant.DEFAULT, props);
+    }
+}
