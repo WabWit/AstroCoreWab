0f5027b7fecce994603dd7bfd0da439075a97f0d
HazeVista <zetateamhazevista@gmail.com>
2026-04-27 07:59:39 +0000

    0.1.5 Haze space suit on spawn

---
+++ src/main/java/com/astro/core/events/SpawnItemsHandler.java
@@ -0,0 +1,41 @@
+package com.astro.core.events;
+
+import com.astro.core.AstroCore;
+import earth.terrarium.adastra.common.registry.ModItems;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.event.entity.EntityJoinLevelEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = AstroCore.MOD_ID)
+public class SpawnItemsHandler {
+
+    @SubscribeEvent
+    public static void onPlayerJoin(EntityJoinLevelEvent event) {
+        if (!(event.getEntity() instanceof ServerPlayer player)) return;
+        if (player.getTags().contains("StartingItems")) return;
+
+        player.getInventory().armor.set(3, new ItemStack(ModItems.SPACE_HELMET.get()));
+
+
+        ItemStack suit = new ItemStack(ModItems.SPACE_SUIT.get());
+        CompoundTag botariumData = new CompoundTag();
+        ListTag storedFluids = new ListTag();
+        CompoundTag fluid = new CompoundTag();
+        fluid.putString("Fluid", "gtceu:oxygen");
+        fluid.putLong("Amount", 2500L);
+        storedFluids.add(fluid);
+        botariumData.put("StoredFluids", storedFluids);
+        botariumData.putInt("Damage", 0);
+        suit.getOrCreateTag().put("BotariumData", botariumData);
+        player.getInventory().armor.set(2, suit);
+
+        player.getInventory().armor.set(1, new ItemStack(ModItems.SPACE_PANTS.get()));
+        player.getInventory().armor.set(0, new ItemStack(ModItems.SPACE_BOOTS.get()));
+
+        player.addTag("StartingItems");
+    }
+}
